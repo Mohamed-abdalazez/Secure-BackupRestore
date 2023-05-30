071935c495c9c645085864a79e50394acea5eafe
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-05-31 01:51:43 +0300

    1) read a specific directory: -> each directory should be backed up in a separate compressed tar file. -> all the files within the directory should be backed up in a separate compressed tar file.

--- Backups/backup.sh
+++ Backups/backup.sh
@@ -9,18 +9,17 @@ TargetBackup=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Backups
 cp -r ${TargetDir} ${TargetBackup}
 
 data=${TargetBackup}/Data
+
 echo ${data}
+files=`ls ${data}`
+# echo ${files}
+for i in $files
+do
+ echo ${i}
+  tar -czvf ${i}.tar.gz ./Data/${i} --remove-files
+  cp -r ${i}.tar.gz ./Data
+  rm  ${i}.tar.gz
+done
 
 tar -czvf ${data}.tar.gz ./Data --remove-files
 
-# files=`ls ${data}`
-# # echo ${files}
-# for i in $files
-# do
-# # echo ${i}
-# # tar -czvf ${i}.tar.gz ${data}/${i}
-# done
-
-
-
-# echo ${data}
