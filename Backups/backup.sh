0d1a82f389a56a416ec064b741459a9641cc2c66
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-05-31 04:16:45 +0300

    validate input #1

--- Backups/backup.sh
+++ Backups/backup.sh
@@ -3,8 +3,25 @@
 # reading the target directory 
 # read -p "Please Enter the path of the target directory: " Target
 
-TargetDir=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Data
-TargetBackup=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Backups
+TargetDir=$1
+TargetBackup=$2
+
+echo $#
+
+if [ $# -eq 2 ]
+then
+echo "ok"
+else
+echo "To be able to use the script, you should do the following:"
+echo "1) directory to be backed up."
+echo "2) directory which should store eventually the backup."
+echo "3) encryption key that you should use to encrypt your backup."
+echo "4) number of days (n) that the script should use to backup only the changed files during the last n days."
+exit 0
+fi
+
+# TargetDir=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Data
+# TargetBackup=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Backups
 
 cp -r ${TargetDir} ${TargetBackup}
 
