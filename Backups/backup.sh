9440ef3e56e08851c3ff754b0618ab1b1d069049
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-05-31 09:24:41 +0300

    Adding the shell format

--- Backups/backup.sh
+++ Backups/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# reading the target directory 
+# reading the target directory
 # read -p "Please Enter the path of the target directory: " Target
 
 TargetDir=$1
@@ -8,16 +8,17 @@ TargetBackup=$2
 
 echo $#
 
-if [ $# -eq 2 ]
-then
-echo "ok"
+
+
+if [ $# -eq 2 ]; then
+  echo "ok"
 else
-echo "To be able to use the script, you should do the following:"
-echo "1) directory to be backed up."
-echo "2) directory which should store eventually the backup."
-echo "3) encryption key that you should use to encrypt your backup."
-echo "4) number of days (n) that the script should use to backup only the changed files during the last n days."
-exit 0
+  echo "To be able to use the script, you should do the following:"
+  echo "1) directory to be backed up."
+  echo "2) directory which should store eventually the backup."
+  echo "3) encryption key that you should use to encrypt your backup."
+  echo "4) number of days (n) that the script should use to backup only the changed files during the last n days."
+  exit 0
 fi
 
 # TargetDir=/home/mohamed/Desktop/Safrot/Projects/Secure-BackupRestore/Data
@@ -28,15 +29,13 @@ cp -r ${TargetDir} ${TargetBackup}
 data=${TargetBackup}/Data
 
 echo ${data}
-files=`ls ${data}`
+files=$(ls ${data})
 # echo ${files}
-for i in $files
-do
- echo ${i}
+for i in $files; do
+  echo ${i}
   tar -czvf ${i}.tar.gz ./Data/${i} --remove-files
   cp -r ${i}.tar.gz ./Data
-  rm  ${i}.tar.gz
+  rm ${i}.tar.gz
 done
 
 tar -czvf ${data}.tar.gz ./Data --remove-files
-
