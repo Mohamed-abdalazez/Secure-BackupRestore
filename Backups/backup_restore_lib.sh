8799b38af31eb1dc415e1a9b8e1cee93fcd91aa4
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-06-02 03:02:17 +0300

    adding some comments

--- Backups/backup_restore_lib.sh
+++ Backups/backup_restore_lib.sh
@@ -42,19 +42,23 @@ validate_backup_params() {
 
 backup() {
 
-  # check if already taken a backup
+  # check if already taken a backup.
   testFile=${TargetBackup}/".testFile.txt"
   if [ -f "$testFile" ]; then
     echo "You've already taken a backup. If anything changes to the original files, will be modified immediately when you run the script again."
     exit 0
   fi
 
+  # get the directory to be backed up to the backup area.
   cp -r ${TargetDir} ${TargetBackup}
+
+  # A new path directory to be backed up in the backup area.
   data=${TargetBackup}/$(basename $TargetDir)
-  # echo ${data}
+
   cd ${data}
   files=$(ls ${data})
 
+  # Compress all files inside the directory using tar.
   for i in $files; do
     # echo ${i}
     fullDate=$(echo $(date) | sed 'y/ /_/')
@@ -64,7 +68,10 @@ backup() {
 
   done
 
+  # creating a directory whose name is equivalent to the date taken in the compression process
+  # creating a Heddin .testFile.txt file to use it to check if already taken a backup or not.
   $(cd .. && mkdir ${fullDate} && touch .testFile.txt)
+
   # changed=$(ls ${TargetDir})
   # echo ${changed}
   # for i in ${changed}; do
