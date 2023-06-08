ae6e91ce70afbf8fbff829116d5609055680eb84
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-06-08 08:42:12 +0300

    Fixed a bug when passing arguments into a validate_backup_params() function.

--- Backups/backup.sh
+++ Backups/backup.sh
@@ -7,5 +7,5 @@ days=$4
 
 source ../backup_restore_lib.sh
 
-validate_backup_params ${TargetDir} ${TargetBackup} ${EncryptionKey} ${days}
+validate_backup_params ${@}
 backup ${TargetDir} ${TargetBackup}
